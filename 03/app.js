06e8a4972d245c0dc308084464c113cae77c8b19
jjksy <jjksy@naver.com>
2020-04-09 15:49:40 +0900

    03

---
+++ 03/app.js
@@ -0,0 +1,32 @@
+//window
+
+console.log(window.outerWidth);
+console.log(window.name);
+// window.open('http://www.naver.com');
+// window.close;
+
+//location
+// location.href = 'url'; //하면 해당 주소로 이동함
+// location.reload(); //reload하는 것
+
+// window.location
+// location
+// // window.alert(1)
+
+//history 브라우저 정보
+        // history.back();
+        // history.forward();
+        // history.go(1); 
+//이전 값의 주소를 가져 올 수는 없음. 개인정보 유출 가능성 있기 때문
+
+//screen
+screen.width
+screen.availWidth
+
+//navigator
+navigator.userAgent
+navigator.language
+navigator.platform
+
+//----------------------------------------------
+//dom 탐색
