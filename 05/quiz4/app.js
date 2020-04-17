6190f8074eb2c1bb67294f23fed4b723aaf6dd53
jjksy <jjksy@naver.com>
2020-04-17 15:19:15 +0900

    Update app.js

---
+++ 05/quiz4/app.js
@@ -0,0 +1,20 @@
+var $body = document.body;
+var $box = document.getElementById('box');
+var moveX = 0;
+var moveY = 0;
+
+function dragBox(){
+
+}
+
+function mouseDown(){
+
+}
+
+function mouseMove(){
+
+}
+
+function mouseUp(){
+
+}
