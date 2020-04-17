736969828fcdf21bbf1120947dd28b8d10eae323
jjksy <jjksy@naver.com>
2020-04-17 15:29:06 +0900

    Update app.js

--- 05/quiz4/app.js
+++ 05/quiz4/app.js
@@ -2,17 +2,26 @@ var $body = document.body;
 var $box = document.getElementById('box');
 var moveX = 0;
 var moveY = 0;
+var moveX2 = 0;
+var moveY2 = 0;
 
-function dragBox(){
-
-}
-
-function mouseDown(){
+function dragBox(x,y){
 
 }
 
-function mouseMove(){
+function mouseDown(event){
+    moveX = event.movementX;
+    moveY = event.movementY;
+}
 
+function mouseMove(event){
+    moveX = event.movementX;
+    moveY = event.movementY;
+
+    moveX2 = event.movementX - moveX;
+    moveY2 = event.movementY - moveY;
+
+    dragBox(moveX2,moveY2);
 }
 
 function mouseUp(){
