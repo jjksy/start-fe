92578cf03865538624d62120cebfa9dc6d5f946f
jjksy <jjksy@naver.com>
2020-04-25 23:10:01 +0900

    06

--- 06/06/app.js
+++ 06/06/app.js
@@ -1,2 +1,15 @@
-var a = 'aa';
-console.log(a);
+/* //var a = 'aa';
+let a = 'aa';
+const b = 'bb';
+var c = '11';
+c = 33;
+// b = 11; // 에러남
+console.log(a,b,c); */
+
+// var str =  'aaaaaaaa
+// sdfs';  //이러면 에러남
+
+const s = `aaaaaaa
+cccccccc
+ddddddddd`;
+console.log(s);
