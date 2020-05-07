7ebb619e10e0fdeb104b7313b6865d829c44dbe9
jjksy <jjksy@naver.com>
2020-05-08 00:06:11 +0900

    07

--- 07/sol_quiz/quiz6.js
+++ 07/sol_quiz/quiz6.js
@@ -2,4 +2,19 @@ const $num = document.getElementById('num');
 const $btn = document.getElementById('btn');
 const $result = document.getElementById('result');
 
-var count = 0;
+function print(event) {
+  //   console.log('click');
+  const result = [];
+  const num = parseInt($num.value, 10);
+
+  if (isNaN(num)) {
+    alert('숫자를 입력하세요');
+    return;
+  }
+  for (let i = 1; i < 10; i++) {
+    result.push(`${num} x ${i} = ${num * i}`);
+  }
+  $result.innerHTML = result.join('<br>');
+}
+
+$btn.addEventListener('click', print);
