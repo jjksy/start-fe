d3226c32bc157b06ee47908f865088409040abf9
jjksy <jjksy@naver.com>
2020-06-13 03:55:33 +0900

    11

--- 11/src/data-manager.js
+++ 11/src/data-manager.js
@@ -2,8 +2,14 @@ function saveData(data) {
   console.log('todos:', data);
   localStorage.setItem('data', JSON.stringify(data));
 }
-function loadData() {
-  const dataString = localStorage.getItem('data') || '[]';
-  return JSON.parse(dataString);
+function loadData(callback) {
+  // const dataString = localStorage.getItem('data') || '[]';
+  // return JSON.parse(dataString);
+  fetch('http://localhost:3000/todos').then(function (response) {
+    response.JSON().then((data) => {
+      // console.log('json data:', data);
+      callback(data);
+    });
+  });
 }
 export { saveData, loadData };
--- 11/src/index.js
+++ 11/src/index.js
@@ -1,23 +1,23 @@
 import { render } from './html-render';
 import formInput from './form-input';
-import './todos.css';
 import { loadData } from './data-manager';
+import './todos.css';
+
 const $result = document.querySelector('#result');
 
-const todos = loadData();
-
-$result.addEventListener('click', (event) => {
-  const { className } = event.target;
-  if (className === 'delete') {
-    const { index } = event.target.parentElement.dataset;
-    todos.splice(index, 1);
-    render(todos);
-  } else if (className === 'toggle-checked') {
-    const { index } = event.target.parentElement.dataset;
-    todos[index].isDone = !todos[index].isDone;
-  }
+const todos = loadData((todos) => {
+  $result.addEventListener('click', (event) => {
+    const { className } = event.target;
+    if (className === 'delete') {
+      const { index } = event.target.parentElement.dataset;
+      todos.splice(index, 1);
+      render(todos);
+    } else if (className === 'toggle-checked') {
+      const { index } = event.target.parentElement.dataset;
+      todos[index].isDone = !todos[index].isDone;
+      render(todos);
+    }
+  });
+  formInput.init(todos);
+  render(todos);
 });
-// console.log($inputForm);
-
-formInput.init(todos);
-render(todos);
