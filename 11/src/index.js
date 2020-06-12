82157a91580eb99d48d394c4396273f90686d3a9
jjksy <jjksy@naver.com>
2020-06-12 15:28:10 +0900

    11

--- 11/src/index.js
+++ 11/src/index.js
@@ -1,1 +1,22 @@
 import './todos.css';
+
+const $inputForm = document.querySelector('#input-form');
+const $input = document.querySelector('#input');
+const $result = document.querySelector('#result');
+
+const todoModel = {
+  id: '',
+  text: '',
+  isDone: false,
+};
+// console.log($inputForm);
+
+$inputForm.addEventListener('submit', (event) => {
+  //   console.log(event);
+  event.preventDefault();
+  const text = $input.value;
+  //   console.log(text);
+  $input.value = '';
+});
+
+$result.innerHTML = '';
