f507e03767180efeba7fb11e978861141c30bcd3
jjksy <jjksy@naver.com>
2020-06-12 15:03:40 +0900

    11

---
+++ 11/webpack.config.js
@@ -0,0 +1,18 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: `${__dirname}/dist`,
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
